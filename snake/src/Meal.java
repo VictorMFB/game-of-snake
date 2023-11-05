e8dbdfd7ef8c985cdb83dd9e04993bf11f514ef5
Elbert <elbertloza293@gmail.com>
2023-11-04 20:08:56 -0400

    Codigo de la comida

--- snake/src/Meal.java
+++ snake/src/Meal.java
@@ -1,3 +1,38 @@
+import java.util.Random;
+
 public class Meal {
-    //La comida que debe negerarse aleatoriamente segun el mapa
-}
+        private int x; //Coordenada X
+        private int y; //Coordenada Y
+    
+        //Constructor para inicializar la posición de la comida
+        public Meal(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    
+        //Métodos para obtener y establecer la posición
+        public int getX() {
+            return x;
+        }
+    
+        public void setX(int x) {
+            this.x = x;
+        }
+    
+        public int getY() {
+            return y;
+        }
+    
+        public void setY(int y) {
+            this.y = y;
+        }
+
+            public void generateRandomPosition(int boardWidth, int boardHeight) {
+                Random rand = new Random();
+                x = rand.nextInt(boardWidth);
+                y = rand.nextInt(boardHeight);
+        }
+
+    }
+    
+
